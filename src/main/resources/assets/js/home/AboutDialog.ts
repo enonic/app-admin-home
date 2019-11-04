281a1afdea70393ae47799897e5faebd03a56a31
Alan Semenov <ase@enonic.com>
2019-11-04 22:03:42 +0100

    Added fetching/appending of license contents to the dialog (#68)

--- src/main/resources/assets/js/home/AboutDialog.ts
+++ src/main/resources/assets/js/home/AboutDialog.ts
@@ -1,15 +1,16 @@
 import Element = api.dom.Element;
+import DivEl = api.dom.DivEl;
 import ModalDialog = api.ui.dialog.ModalDialog;
+import i18n = api.util.i18n;
+
+const licenseUrl: string = 'https://raw.githubusercontent.com/enonic/xp/master/NOTICE.txt';
 
 export const create = (): ModalDialog => {
     const aboutDialog = new ModalDialog({skipTabbable: true});
     const aboutDialogContent = getAboutDialogContent();
     aboutDialog.addClass('xp-about-dialog');
     aboutDialogContent.onAdded(() => {
-        document.getElementById('xp-license-info-link').addEventListener(
-            'click',
-            (e: Event) => toggleLicenseInfo(e)
-        );
+        createLicenseInfoContainer();
     });
 
     aboutDialog.appendChildToContentPanel(aboutDialogContent);
@@ -17,72 +18,67 @@ export const create = (): ModalDialog => {
     return aboutDialog;
 };
 
-/*
-const fetchLicenses = () => {
-    fetch()
+
+const fetchLicenses = (): Promise<string> => {
+    return fetch(licenseUrl)
+        .then(response => response.text())
+        .catch(() => {
+            return 'Failed to fetch license info';
+        });
 };
-*/
 
 
-const appendLicenseInfo = () => {
-    const container = Element.fromHtmlElement(document.querySelector('.xp-license-info-container'), true);
-    if (container.getChildren().length) {
+const createLicenseInfoContainer = () => {
+    const outerContainer = Element.fromHtmlElement(document.querySelector('.xp-about-dialog-license'));
+    const button = new api.ui.button.Button(i18n('home.dashboard.about.dialog.licensing'));
+
+    const licenseInfoHeader = new DivEl('xp-license-info-header');
+    licenseInfoHeader.setHtml(i18n('home.dashboard.about.dialog.licensing.title'));
+
+    const licenseInfoContainer = new DivEl('xp-license-info-body');
+    //licenseInfoBody.setHtml('Fetched license info here...');
+
+    button.onClicked((e: MouseEvent) => toggleLicenseInfo(e, outerContainer, licenseInfoContainer));
+
+    outerContainer.appendChildren(button, licenseInfoHeader, licenseInfoContainer);
+};
+
+const toggleLicenseInfo = (e: MouseEvent, outerContainer: Element, licenseInfoContainer: DivEl) => {
+    if (!licenseInfoContainer.getHtml()) {
+
+        fetchLicenses().then((licenseText: string) => {
+            licenseInfoContainer.setHtml(licenseText);
+
+            outerContainer.toggleClass('expanded');
+        });
+
         return;
     }
 
-    const licenseInfoHeader = new api.dom.DivEl('xp-license-info-header');
-    licenseInfoHeader.setHtml('Enonic XP is licensed under Gnu Public License v3 (GPL3)');
-
-    const licenseInfoBody = new api.dom.DivEl('xp-license-info-body');
-    licenseInfoBody.setHtml('Fetched license info here...');
-
-    container.appendChildren(licenseInfoHeader, licenseInfoBody);
-};
-
-
-const toggleLicenseInfo = (e: Event) => {
-    const container = Element.fromHtmlElement(document.querySelector('.xp-about-dialog-license'));
-
-    appendLicenseInfo();
-
-    container.toggleClass('expanded');
-
-    e.stopPropagation();
-    e.preventDefault();
+    outerContainer.toggleClass('expanded');
 };
 
 const getAboutDialogContent = () => {
-    const i18n = api.util.i18n;
-
-    const html =
-        '<div class="xp-about-dialog-content">' +
-        '    <div class="xp-about-dialog-app-icon">' +
-        '        <img src="' +
-        CONFIG.assetsUri +
-        '/icons/app-icon.svg">' +
-        '    </div>' +
-        '    <h1>Enonic XP</h1>' +
-        '    <div class="xp-about-dialog-version-block">' +
-        '        <span class="xp-about-dialog-version">' +
-        CONFIG.xpVersion +
-        '</span>&nbsp;&nbsp;' +
-        '        <a href="https://developer.enonic.com/docs/xp/" target="_blank">' +
-        i18n('home.dashboard.about.dialog.whatsnew') +
-        '</a>' +
-        '    </div>' +
-        '    <div class="xp-about-dialog-text">' +
-        i18n(
-            'home.dashboard.about.dialog.text',
-            '<span style="color: red;">♥</span>'
-        ) +
-        '    </div>' +
-        '    <div class="xp-about-dialog-license">' +
-        '       <a id="xp-license-info-link" href="#">' +
-        i18n('home.dashboard.about.dialog.licensing') +
-        '       </a>' +
-        '       <div class="xp-license-info-container"></div>' +
-        '    </div>' +
-        '</div>';
+    const html = `
+        <div class="xp-about-dialog-content">
+            <div class="xp-about-dialog-app-icon">
+                <img src="${CONFIG.assetsUri}/icons/app-icon.svg">
+            </div>
+            <h1>Enonic XP</h1>
+            <div class="xp-about-dialog-version-block">
+                <span class="xp-about-dialog-version">${CONFIG.xpVersion}</span>&nbsp;&nbsp;
+                <a href="https://developer.enonic.com/docs/xp/" target="_blank">
+                    ${i18n('home.dashboard.about.dialog.whatsnew')}
+                </a>
+            </div>
+            <div class="xp-about-dialog-text">
+                ${i18n(
+                    'home.dashboard.about.dialog.text',
+                    '<span style="color: red;">♥</span>'
+                )}
+            </div>
+            <div class="xp-about-dialog-license"></div>
+        </div>`;
 
     const element = api.dom.Element.fromString(html);
     return element;
