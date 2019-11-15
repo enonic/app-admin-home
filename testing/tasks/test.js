117312f775e42ec33196881a2972d1d800b2dd03
Siarhei Hauruseu <sig@enonic.com>
2019-11-15 11:28:45 +0300

    version of selenium-server updated

--- testing/tasks/test.js
+++ testing/tasks/test.js
@@ -1,33 +1,9 @@
 const path = require('path');
-const fs = require('fs');
 const Mocha = require('mocha');
 const selenium = require('selenium-standalone');
 const globby = require('globby');
 const testFilesGlob = './specs/**/*.js';
 
-
-function runSelenium() {
-    selenium.install(
-        {logger: msg => console.log(msg)},
-        function (error) {
-            if (error) {
-                return error;
-            }
-            selenium.start((error, child) => {
-                if (error) {
-                    return error;
-                }
-                selenium.child = child;
-            });
-        }
-    );
-}
-
-function stopSelenuim() {
-    selenium.child.kill();
-}
-
-// runSelenium();
 
 const mocha = new Mocha({
     reporter: 'mochawesome',
@@ -37,18 +13,63 @@ const mocha = new Mocha({
     }
 });
 
-(async () => {
-    const paths = await globby([testFilesGlob]);
+function stopSelenuim() {
+    selenium.child.kill();
+}
 
-    paths.forEach(function(filePath){
+async function runTests() {
+    const paths = await globby([testFilesGlob]);
+    paths.forEach(function (filePath) {
         console.log(filePath);
         mocha.addFile(filePath);
     });
 
-    mocha.run(exitCode => {
-        // stopSelenuim();
+    mocha.run(function (exitCode) {
+        stopSelenuim();
         if (exitCode !== 0) {
             process.exit(exitCode);
         }
     });
-})();
+}
+
+function runSeleniumTests() {
+    selenium.install(
+        {
+            version: '3.9.0',
+            baseURL: 'https://selenium-release.storage.googleapis.com',
+            drivers: {
+                chrome: {
+                    version: '76.0.3809.126',
+                    arch: process.arch,
+                    baseURL: 'https://chromedriver.storage.googleapis.com'
+                }
+            },
+
+            logger: msg => console.log(msg)
+        },
+        function (error) {
+            if (error) {
+                console.log("Selenium server is not started! 1" + error);
+                return error;
+            }
+            selenium.start({
+                version: '3.9.0',
+                drivers: {
+                    chrome: {
+                        version: '76.0.3809.126'
+                    }
+                }
+            }, (error, child) => {
+
+                if (error) {
+                    console.log("Selenium server is not started 2 !" + error);
+                    return error;
+                }
+                console.log("Selenium server is started!")
+                selenium.child = child;
+                runTests();
+            });
+        }
+    );
+}
+runSeleniumTests();
