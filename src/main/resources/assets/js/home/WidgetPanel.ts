87426c2f15f11b410214abc0d1f0e9d52551b3d3
Alan Semenov <ase@enonic.com>
2022-09-13 14:43:42 +0200

    Add transition on widget load (#769)

--- src/main/resources/assets/js/home/WidgetPanel.ts
+++ src/main/resources/assets/js/home/WidgetPanel.ts
@@ -5,6 +5,8 @@ import {DivEl} from '@enonic/lib-admin-ui/dom/DivEl';
 import {H5El} from '@enonic/lib-admin-ui/dom/H5El';
 import {Element} from '@enonic/lib-admin-ui/dom/Element';
 import {DefaultErrorHandler} from '@enonic/lib-admin-ui/DefaultErrorHandler';
+import {LoadMask} from '@enonic/lib-admin-ui/ui/mask/LoadMask';
+import {Exception} from '@enonic/lib-admin-ui/Exception';
 
 export class WidgetPanel
     extends DivEl {
@@ -13,21 +15,7 @@ export class WidgetPanel
         super('widget-panel');
     }
 
-    layout(): Q.Promise<boolean> {
-        const deferred = Q.defer<boolean>();
-        this.fetchWidgets()
-            .then((widgetEls: Element[]) => {
-                this.appendChildren(...widgetEls);
-                deferred.resolve(widgetEls.length > 0);
-            })
-            .catch(() => deferred.reject());
-
-        return deferred.promise;
-    }
-
-    private fetchWidgets(): Q.Promise<Element[]> {
-        const deferred = Q.defer<Element[]>();
-
+    fetchAndAppendWidgets(): void {
         new GetDashboardWidgetsRequest().fetchWidgets()
             .then((widgets: Widget[]) => {
                 let baseUrl = document.location.href;
@@ -35,41 +23,78 @@ export class WidgetPanel
                     baseUrl += '/';
                 }
 
-                const fetchWidgetPromises = [];
-                widgets.forEach((widget: Widget) => fetchWidgetPromises.push(this.fetchWidget(widget, baseUrl)));
+                widgets.forEach((widget: Widget) => this.fetchAndAppendWidget(widget, baseUrl));
+            })
+            .catch(DefaultErrorHandler.handle);
+    }
 
-                Q.all(fetchWidgetPromises)
-                    .then((widgetElements: Element[]) => deferred.resolve(widgetElements))
+    private fetchAndAppendWidget(widget: Widget, baseUrl: string) {
+        const widgetWidthCls: string = widget.getConfig()['width'] || 'auto';
+        const widgetPlaceholder: Element = new DivEl(`widget-placeholder ${widgetWidthCls}`)
+                                            .appendChild(new H5El('widget-header').setHtml(widget.getDisplayName()));
+        this.appendChild(widgetPlaceholder);
+
+        widgetPlaceholder.render()
+            .then(() => {
+                const loadMask: LoadMask = new LoadMask(widgetPlaceholder);
+                loadMask.show();
+
+                fetch(baseUrl + widget.getUrl())
+                    .then(response => response.text())
+                    .then((html: string) => {
+                        const sanitisedWidgetEl: Element = this.getSanitisedWidget(widget, html);
+
+                        this.injectWidgetAssets(sanitisedWidgetEl)
+                            .then(() => {
+                                widgetPlaceholder.appendChild(sanitisedWidgetEl.addClass('widget-contents'));
+                                widgetPlaceholder.addClass('loaded');
+                            })
+                            .catch(DefaultErrorHandler.handle)
+                            .finally(() => loadMask.hide());
+                    })
                     .catch(DefaultErrorHandler.handle);
             })
             .catch(DefaultErrorHandler.handle);
-
-        return deferred.promise;
     }
 
-    private fetchWidget(widget: Widget, baseUrl: string): Q.Promise<Element> {
-        const deferred = Q.defer<Element>();
-        widgetEl.prependChild(new H5El('widget-header').setHtml(widget.getDisplayName()));
-        fetch(baseUrl + widget.getUrl())
-            .then(response => response.text())
-            .then((html: string) => {
-                const widgetWidthCls: string = widget.getConfig()['width'] || 'auto';
-                const widgetEl: Element = Element.fromCustomarilySanitizedString(
-                    html,
-                    true,
-                    {
-                        addTags: [
-                            'widget',
-                            'link',
-                        ],
-                        addAttributes: ['target'],
-                    },
-                );
-                widgetEl.setClass(widgetWidthCls);
-                deferred.resolve(widgetEl);
-            })
-            .catch(DefaultErrorHandler.handle);
+    private getSanitisedWidget(widget: Widget, widgetHtml: string): Element {
+        const widgetEl: Element = Element.fromCustomarilySanitizedString(
+            widgetHtml,
+            true,
+            {
+                addTags: [
+                    'widget',
+                    'link', // allow widget assets
+                ],
+                addAttributes: ['target'],  // allow opening links in a new window
+            },
+        );
 
-        return deferred.promise;
+        if ('WIDGET' !== widgetEl.getHTMLElement().nodeName) {
+            throw new Exception(`${widget.getWidgetDescriptorKey().toString()} is missing <widget> root element`);
+        }
+
+        return widgetEl;
+    }
+
+    private injectWidgetAssets(widgetEl: Element): Q.Promise<void[]>  {
+
+        const linkHTMLElements = widgetEl.getHTMLElement().getElementsByTagName('link');
+
+        if (!linkHTMLElements.length) {
+            return Q.resolve();
+        }
+
+        const promises: Q.Promise<void>[] = [];
+        for (let i = 0; i < linkHTMLElements.length; i++) {
+            const deferred = Q.defer<void>();
+            promises.push(deferred.promise);
+            const el = linkHTMLElements.item(i);
+            el.remove();
+            el.onload = () => deferred.resolve();
+            document.head.appendChild(el);
+        }
+
+        return Q.all(promises);
     }
 }
