d9200ff3ef403f085087371d3981dcafacecd3cc
Alan Semenov <ase@enonic.com>
2023-01-05 14:18:16 +0100

    Refresh dashboard widgets on app events (#770)

--- src/main/resources/assets/js/home/WidgetPanel.ts
+++ src/main/resources/assets/js/home/WidgetPanel.ts
@@ -7,16 +7,86 @@ import {Element as LibAdminElement} from '@enonic/lib-admin-ui/dom/Element';
 import {DefaultErrorHandler} from '@enonic/lib-admin-ui/DefaultErrorHandler';
 import {LoadMask} from '@enonic/lib-admin-ui/ui/mask/LoadMask';
 import {Exception} from '@enonic/lib-admin-ui/Exception';
+import {ApplicationEvent, ApplicationEventType} from '@enonic/lib-admin-ui/application/ApplicationEvent';
 
 export class WidgetPanel
     extends DivEl {
 
     private static widgetUrlPrefix: string;
 
+    private static widgetCache: Map<string, string[]> = new Map<string, string[]>();
+
+    private widgetCount = 0;
+
     constructor() {
         super('widget-panel');
 
         WidgetPanel.widgetUrlPrefix = WidgetPanel.getUrlPrefix();
+        this.addApplicationsListeners();
+    }
+
+    appendWidgets(): void {
+        this.fetchWidgets()
+            .then((widgets: Widget[]) => {
+                if (widgets.length === this.widgetCount) {
+                    return;
+                }
+
+                if (this.widgetCount > 0) {
+                    this.removeChildren();
+                }
+                this.widgetCount = widgets.length;
+                widgets
+                    .sort((w1: Widget, w2: Widget) => this.sortByOrder(w1, w2))
+                    .forEach((widget: Widget) => this.appendWidget(widget));
+            })
+            .catch(DefaultErrorHandler.handle);
+    }
+
+    private addApplicationsListeners(): void {
+        ApplicationEvent.on((e: ApplicationEvent) => {
+            const applicationKey: string = e.getApplicationKey().toString();
+            if (this.isApplicationDeactivated(e)) {
+                this.handleApplicationDeactivated(applicationKey);
+            } else if (this.isApplicationActivated(e)) {
+                this.handleApplicationActivated(applicationKey);
+            }
+        });
+    }
+
+    private isApplicationDeactivated(e: ApplicationEvent): boolean {
+        const eventType: ApplicationEventType = e.getEventType();
+        return ApplicationEventType.STOPPED === eventType || ApplicationEventType.UNINSTALLED === eventType;
+    }
+
+    private handleApplicationDeactivated(applicationKey: string): void {
+        const widgetKeys: string[] = WidgetPanel.widgetCache.get(applicationKey);
+        if (widgetKeys) {
+            this.removeWidgets(widgetKeys);
+            WidgetPanel.widgetCache.delete(applicationKey);
+        }
+    }
+
+    private isApplicationActivated(e: ApplicationEvent): boolean {
+        return ApplicationEventType.STARTED === e.getEventType();
+    }
+
+    private handleApplicationActivated(applicationKey: string): void {
+        if (WidgetPanel.widgetCache.has(applicationKey)) {
+            this.updateWidgets(applicationKey);
+        } else {
+            this.appendWidgets();
+        }
+    }
+
+    private removeWidgets(widgetKeys: string[]): void {
+        widgetKeys.forEach((widgetKey: string) => {
+            const widgetElement = this.findChildById(widgetKey, true);
+            if (widgetElement) {
+                this.widgetCount--;
+                widgetElement.remove();
+            }
+        });
     }
 
     private static getUrlPrefix(): string {
@@ -37,28 +107,53 @@ export class WidgetPanel
         return widgetOrder ? parseInt(widgetOrder) : Number.MAX_VALUE;
     }
 
-    fetchAndAppendWidgets(): void {
-        new GetDashboardWidgetsRequest().fetchWidgets()
+    private fetchWidgets(): Q.Promise<Widget[]> {
+        return new GetDashboardWidgetsRequest().fetchWidgets();
+    }
+
+    private updateWidgets(applicationKey: string): void {
+        this.fetchWidgets()
             .then((widgets: Widget[]) => {
+                if (widgets.length !== this.widgetCount) {
+                    this.appendWidgets();
+                    return;
+                }
+
                 widgets
-                    .sort((w1: Widget, w2: Widget) => this.sortByOrder(w1, w2))
-                    .forEach((widget: Widget) => this.fetchAndAppendWidget(widget));
+                    .filter((widget: Widget) => widget.getWidgetDescriptorKey().getApplicationKey().toString() === applicationKey)
+                    .forEach((widget: Widget) => this.updateWidget(widget));
             })
             .catch(DefaultErrorHandler.handle);
     }
 
-    private fetchAndAppendWidget(widget: Widget) {
-        const widgetPlaceholder: LibAdminElement = this.createWidgetPlaceholder(widget);
-        this.appendChild(widgetPlaceholder);
+    private appendWidget(widget: Widget, existingContainer?: LibAdminElement) {
+        const widgetContainer: LibAdminElement = existingContainer || this.createWidgetPlaceholder(widget);
+        if (!existingContainer) {
+            this.appendChild(widgetContainer);
+        }
 
-        widgetPlaceholder.render()
-            .then(() => {
-                const loadMask: LoadMask = new LoadMask(widgetPlaceholder);
-                loadMask.show();
-
-                return this.fetchWidget(widget, widgetPlaceholder).finally(() => loadMask.hide());
-            })
+        widgetContainer.render()
+            .then(() => this.addWidget(widget, widgetContainer))
             .catch(DefaultErrorHandler.handle);
+    }
+
+    private updateWidget(widget: Widget) {
+        const widgetContainer = this.findChildById(widget.getWidgetDescriptorKey().toString());
+        if (!widgetContainer) {
+            return;
+        }
+
+        const newWidgetContainer = this.createWidgetPlaceholder(widget);
+        widgetContainer.replaceWith(newWidgetContainer);
+
+        this.appendWidget(widget, newWidgetContainer);
+    }
+
+    private addWidget(widget: Widget, widgetContainer: LibAdminElement): void {
+        const loadMask: LoadMask = new LoadMask(widgetContainer);
+        loadMask.show();
+
+        this.fetchAndRenderWidget(widget, widgetContainer).finally(() => loadMask.hide());
     }
 
     private createWidgetPlaceholder(widget: Widget): LibAdminElement {
@@ -67,6 +162,8 @@ export class WidgetPanel
         const widgetPlaceholder: LibAdminElement =
             new DivEl(`widget-placeholder width-${widthCls.toLowerCase()}${styleCls ? ` style-${styleCls.toLowerCase()}` : ''}`);
 
+        widgetPlaceholder.setId(widget.getWidgetDescriptorKey().toString());
+
         if (styleCls !== 'custom') {
             widgetPlaceholder.appendChild(new H5El('widget-header').setHtml(widget.getDisplayName()));
         }
@@ -74,17 +171,37 @@ export class WidgetPanel
         return widgetPlaceholder;
     }
 
-    private fetchWidget(widget: Widget, widgetContainer: LibAdminElement): Promise<void> {
+    private static cacheWidgetKey(widget: Widget): void {
+        const applicationKey: string = widget.getWidgetDescriptorKey().getApplicationKey().toString();
+        const widgetKey: string = widget.getWidgetDescriptorKey().toString();
+
+        const cachedWidgetKeys: string[] = WidgetPanel.widgetCache.get(applicationKey) || [];
+        if (cachedWidgetKeys.includes(widgetKey)) {
+            return;
+        }
+
+        cachedWidgetKeys.push(widgetKey);
+        WidgetPanel.widgetCache.set(applicationKey, cachedWidgetKeys);
+    }
+
+    private fetchAndRenderWidget(widget: Widget, widgetContainer: LibAdminElement): Promise<void> {
         return fetch(WidgetPanel.widgetUrlPrefix + widget.getUrl())
             .then(response => response.text())
             .then((html: string) => {
-                const sanitisedWidgetEl: LibAdminElement = this.getSanitisedWidget(widget, html);
+                WidgetPanel.cacheWidgetKey(widget);
+                return this.renderWidget(widget, widgetContainer, html);
+            });
+    }
 
-                return this.injectWidgetAssets(sanitisedWidgetEl)
-                    .then(() => {
-                        widgetContainer.appendChild(sanitisedWidgetEl.addClass('widget-contents')).addClass('loaded');
-                        return Q.resolve();
-                    });
+    private renderWidget(widget: Widget, widgetContainer: LibAdminElement, html: string): Q.Promise<void> {
+        const sanitisedWidgetEl: LibAdminElement = this.getSanitisedWidget(widget, html);
+
+        return this.injectWidgetAssets(sanitisedWidgetEl)
+            .then(() => {
+                widgetContainer
+                    .appendChild(sanitisedWidgetEl.addClass('widget-contents'))
+                    .addClass('loaded');
+                return Q.resolve();
             });
     }
 
