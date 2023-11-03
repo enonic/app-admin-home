40d074e8f3becb0df1cd867163863e93fc18964a
sgauruseu <sig@eninic.com>
2023-11-03 11:55:09 +0300

    ui-tests - install app-timeout 1 min

--- testing/wdio.chrome.conf.js
+++ testing/wdio.chrome.conf.js
@@ -4,6 +4,9 @@ const {TimelineService} = require('wdio-timeline-reporter/timeline-service');
 const file = path.join(__dirname, '/browser.properties');
 const properties = PropertiesReader(file);
 const browser_version = properties.get('browser.version');
+const width = properties.get('browser.width');
+const height = properties.get('browser.height');
+
 exports.config = {
 
     specs: [
@@ -20,7 +23,7 @@ exports.config = {
                 "--headless", "--disable-gpu", "--no-sandbox",
                 "--lang=en",
                 '--disable-extensions',
-                'window-size=1970,1000'
+                `window-size=${width},1100`
             ]
         }
     }],
