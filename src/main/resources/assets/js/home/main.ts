2dec3ef649799b26114c41290f9630742abc0bb9
alansemenov <ase@enonic.com>
2025-01-20 12:50:58 +0100

    No fade-in effect on the background image #1522

--- src/main/resources/assets/js/home/main.ts
+++ src/main/resources/assets/js/home/main.ts
@@ -22,13 +22,12 @@ const loadDOM = (): Q.Promise<void> => {
     return DOMLoaded.promise;
 };
 
-const showBackgroundImage = (): Q.Promise<void> => {
+const showBackgroundImage = () => {
     const containerEl = document.getElementById(containerId);
     if (!containerEl) {
         throw new Error('Main container not found!');
     }
 
-    const deferred: Q.Deferred<void> = Q.defer<void>();
     const container: Element = Element.fromHtmlElement(containerEl);
     const divEl = new DivEl('lazy-image empty');
     divEl.setId('background-image');
@@ -40,11 +39,8 @@ const showBackgroundImage = (): Q.Promise<void> => {
         divEl.getEl().setAttribute('style', `background-image: url('${imgEl.getSrc()}')`);
         imgEl.remove();
         divEl.removeClass('empty');
-        deferred.resolve();
     });
     container.appendChild(imgEl);
-
-    return deferred.promise;
 };
 
 const initConfig = (configScriptId: string) => {
@@ -124,6 +120,6 @@ void (async () => {
 
     await loadDOM();
     initConfig(configScriptId);
-    await showBackgroundImage();
+    showBackgroundImage();
     startApplication();
 })();
