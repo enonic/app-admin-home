78a500fe17953f6dd366991ba1368d631dacd3aa
rymsha <sry@enonic.com>
2025-05-07 17:02:06 +0200

    all tools must have display name

--- src/main/java/com/enonic/xp/app/main/GetAdminToolsScriptBean.java
+++ src/main/java/com/enonic/xp/app/main/GetAdminToolsScriptBean.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 
 import com.enonic.xp.admin.tool.AdminToolDescriptor;
 import com.enonic.xp.admin.tool.AdminToolDescriptorService;
+import com.enonic.xp.app.ApplicationKey;
 import com.enonic.xp.context.ContextAccessor;
 import com.enonic.xp.i18n.LocaleService;
 import com.enonic.xp.script.bean.BeanContext;
@@ -21,6 +22,8 @@ public final class GetAdminToolsScriptBean
 
     private LocaleService localeService;
 
+    private ApplicationKey appMainKey;
+
     public List<MapSerializable> execute( final List<String> locales )
     {
         final PrincipalKeys principals = ContextAccessor.current().
@@ -28,18 +31,20 @@ public final class GetAdminToolsScriptBean
             getPrincipals();
         final StringTranslator stringTranslator = new StringTranslator( this.localeService, locales.stream().map( Locale::forLanguageTag ).toList() );
 
-        return adminToolDescriptorService.getAllowedAdminToolDescriptors( principals ).
-            stream().
-                sorted(Comparator.comparing(AdminToolDescriptor::getDisplayName)).
-            map( adminToolDescriptor -> new AdminToolMapper( adminToolDescriptor,
-                                                             adminToolDescriptorService.getIconByKey( adminToolDescriptor.getKey() ),
-                                                             stringTranslator ) ).
-            collect( Collectors.toList() );
+        return adminToolDescriptorService.getAllowedAdminToolDescriptors( principals )
+            .stream()
+            .filter( d -> !appMainKey.equals( d.getApplicationKey() )  )
+            .sorted( Comparator.nullsLast( Comparator.comparing( AdminToolDescriptor::getDisplayName ) ) )
+            .map( adminToolDescriptor -> new AdminToolMapper( adminToolDescriptor,
+                                                              adminToolDescriptorService.getIconByKey( adminToolDescriptor.getKey() ),
+                                                              stringTranslator ) )
+            .collect( Collectors.toList() );
     }
 
     @Override
     public void initialize( final BeanContext context )
     {
+        this.appMainKey = context.getApplicationKey();
         this.adminToolDescriptorService = context.getService( AdminToolDescriptorService.class ).get();
         this.localeService = context.getService( LocaleService.class ).get();
     }
