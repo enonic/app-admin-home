bf2d41a78b71a11e0e6a39b16316f482ba437a8b
rymsha <sry@enonic.com>
2026-02-18 16:31:14 +0100

    Non-localized Admin Tools break menu #1754

--- src/main/java/com/enonic/xp/app/main/AdminToolMapper.java
+++ src/main/java/com/enonic/xp/app/main/AdminToolMapper.java
@@ -1,6 +1,9 @@
 package com.enonic.xp.app.main;
 
+import java.util.Optional;
+
 import com.enonic.xp.admin.tool.AdminToolDescriptor;
+import com.enonic.xp.i18n.MessageBundle;
 import com.enonic.xp.script.serializer.MapGenerator;
 import com.enonic.xp.script.serializer.MapSerializable;
 
@@ -11,13 +14,13 @@ final class AdminToolMapper
 
     private final String icon;
 
-    private final StringTranslator stringTranslator;
+    private final MessageBundle messages;
 
-    AdminToolMapper( final AdminToolDescriptor value, final String icon, final StringTranslator stringTranslator )
+    AdminToolMapper( final AdminToolDescriptor value, final String icon, final MessageBundle messages )
     {
         this.value = value;
         this.icon = icon;
-        this.stringTranslator = stringTranslator;
+        this.messages = messages;
     }
 
     private void serialize( final MapGenerator gen, final AdminToolDescriptor value )
@@ -26,11 +29,10 @@ final class AdminToolMapper
         gen.value( "application", value.getKey().getApplicationKey().toString() );
         gen.value( "name", value.getKey().getName() );
         gen.end();
-
-        gen.value( "displayName", this.stringTranslator.localize( value.getKey().getApplicationKey(), value.getDisplayNameI18nKey(),
-                                                                  value.getDisplayName() ) );
-        gen.value( "description", this.stringTranslator.localize( value.getKey().getApplicationKey(), value.getDescriptionI18nKey(),
-                                                                  value.getDescription() ) );
+        gen.value( "displayName",
+                   Optional.ofNullable( value.getDisplayNameI18nKey() ).map( messages::localize ).orElseGet( value::getDisplayName ) );
+        gen.value( "description",
+                   Optional.ofNullable( value.getDescription() ).map( messages::localize ).orElseGet( value::getDisplayName ) );
         gen.value( "icon", icon );
     }
 
